92a9de27b59528bc9635033094588d96dca0c1ce
GlacierColt <99321198+GlacierColt@users.noreply.github.com>
2025-05-28 15:10:08 +0200

    Added user model

---
+++ app/src/main/java/com/example/mandelamoney/User.java
@@ -0,0 +1,50 @@
+package com.example.mandelamoney;
+
+import static java.lang.Math.abs;
+
+public class User {
+    private final String userEmail;
+    private String userPassword;
+    private double userBalance;
+    private final int userType;
+    public User(String userEmail, String userPassword, double userBalance, int userType) {
+        this.userEmail = userEmail;
+        this.userPassword = userPassword;
+        this.userBalance = userBalance;
+        this.userType = userType;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public double getUserBalance() {
+        return userBalance;
+    }
+
+    public int getUserType() {
+        return userType;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+
+    public void decreaseBalance(double amount) {
+        double newUserBalance = userBalance - abs(amount);
+        if (newUserBalance >= 0) {
+            userBalance = newUserBalance;
+        } else {
+            throw new RuntimeException("Invalid Entry: User balance can't be negative");
+        }
+    }
+
+    public void increaseBalance(double amount) {
+        userBalance = userBalance + abs(amount);
+    }
+
+}
