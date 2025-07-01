bb02aa53c9104173266eafc81dcf59e14674e1e7
GlacierColt <99321198+glaciercolt@users.noreply.github.com>
2025-07-01 09:22:56 +0200

    Complete View and UI for Business Create Account

--- app/src/main/java/com/example/mandelamoney/CreateAccount_EnterBusinessDetailsActivity.java
+++ app/src/main/java/com/example/mandelamoney/CreateAccount_EnterBusinessDetailsActivity.java
@@ -1,6 +1,11 @@
 package com.example.mandelamoney;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.InputType;
+import android.text.TextWatcher;
+import android.text.method.PasswordTransformationMethod;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -12,11 +17,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.concurrent.atomic.AtomicReference;
+
 public class CreateAccount_EnterBusinessDetailsActivity extends AppCompatActivity implements ICreateBusinessAccount{
 
     private CreateAccountController controller;
-    TextView txtPasswordError;
-    TextView txtDetailError;
+    private TextView txtPasswordError;
+    private TextView txtDetailError;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,9 +51,16 @@ public class CreateAccount_EnterBusinessDetailsActivity extends AppCompatActivit
         EditText tbxPasswordReenter = findViewById(R.id.tbx_password_reenter_createbusinessaccount);
         ImageView imgPasswordIcon = findViewById(R.id.img_password_icon_createbusinessaccount);
         ImageView imgPasswordRenterIcon = findViewById(R.id.img_password_reenter_createbusinessaccount);
-        //txtPasswordError = findViewById(R.id.txt_error_password_createbusinessaccount);
-       // txtDetailError = findViewById(R.id.txt_error_details_createbusinessaccount);
+        EditText tbxEmail = findViewById(R.id.tbx_email_createbusinessaccount);
+        EditText tbxName = findViewById(R.id.tbx_name_createbusinessaccount);
+        EditText tbxVAT = findViewById(R.id.tbx_vat_createbusinessaccount);
+        EditText tbxPhone = findViewById(R.id.tbx_phone_createbusinessaccount);
+        txtPasswordError = findViewById(R.id.txt_error_password_createbusinessaccount);
+        txtDetailError = findViewById(R.id.txt_error_details_createbusinessaccount);
         configureCancelButton(btnCancel);
+        configurePasswordVisibility(imgPasswordIcon, tbxPassword);
+        configurePasswordVisibility(imgPasswordRenterIcon, tbxPasswordReenter);
+        configureCreateAccountButton(btnCreateAccount, tbxEmail, tbxName, tbxVAT, tbxPhone, tbxPassword, tbxPasswordReenter);
     }
 
     private void configureCancelButton(TextView btnCancel) {
@@ -55,23 +69,76 @@ public class CreateAccount_EnterBusinessDetailsActivity extends AppCompatActivit
         });
     }
 
+    private void configureCreateAccountButton(Button btnCreateAccount, EditText tbxEmail, EditText tbxName, EditText tbxVAT, EditText tbxPhone, EditText tbxPassword, EditText tbxPasswordReenter) {
+        btnCreateAccount.setOnClickListener((view) -> {
+            controller.handleCreateBusinessUser(String.valueOf(tbxEmail.getText()), String.valueOf(tbxName.getText()), String.valueOf(tbxVAT.getText()), String.valueOf(tbxPhone.getText()), String.valueOf(tbxPassword.getText()), String.valueOf(tbxPasswordReenter.getText()));
+        });
+    }
+
     @Override
     public void showPasswordError(String message) {
-
+        txtPasswordError.setText(message);
+        txtPasswordError.setVisibility(View.VISIBLE);
     }
 
     @Override
     public void hidePasswordError() {
-
+        txtPasswordError.setVisibility(View.GONE);
     }
 
     @Override
     public void showDetailError(String message) {
-
+        txtDetailError.setText(message);
+        txtDetailError.setVisibility(View.VISIBLE);
     }
 
     @Override
     public void hideDetailError() {
+        txtDetailError.setVisibility(View.GONE);
+    }
 
+    private void configurePasswordVisibility(ImageView imgPasswordIcon, EditText tbxUserPassword) {
+        tbxUserPassword.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+        tbxUserPassword.setTransformationMethod(new PasswordTransformationMethod());
+        imgPasswordIcon.setImageResource(R.drawable.img_password_icon);
+
+        AtomicReference<Boolean> isPasswordVisible = new AtomicReference<>(false);
+
+        tbxUserPassword.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (s.length() > 0) {
+                    imgPasswordIcon.setImageResource(isPasswordVisible.get() ?
+                            R.drawable.img_eye_closed : R.drawable.img_eye_open);
+                } else {
+                    imgPasswordIcon.setImageResource(R.drawable.img_password_icon);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {}
+        });
+
+        imgPasswordIcon.setOnClickListener(view -> {
+            if (tbxUserPassword.getText().length() == 0) {
+                return;
+            }
+
+            boolean temp = !isPasswordVisible.get();
+            isPasswordVisible.set(temp);
+
+            if (isPasswordVisible.get()) {
+                tbxUserPassword.setTransformationMethod(null);
+                imgPasswordIcon.setImageResource(R.drawable.img_eye_closed);
+            } else {
+                tbxUserPassword.setTransformationMethod(new PasswordTransformationMethod());
+                imgPasswordIcon.setImageResource(R.drawable.img_eye_open);
+            }
+
+            tbxUserPassword.setSelection(tbxUserPassword.getText().length());
+        });
     }
 }
