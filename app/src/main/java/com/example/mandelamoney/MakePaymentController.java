4a8814ffe0e59e87f116c57536ab85baf3ed58ed
GlacierColt <lapel.tidbits.0c@icloud.com>
2025-07-15 20:38:23 +0200

    Controller, Functionality for UI (ConfirmPayment - MakePayment)

--- app/src/main/java/com/example/mandelamoney/MakePaymentController.java
+++ app/src/main/java/com/example/mandelamoney/MakePaymentController.java
@@ -1,4 +1,30 @@
 package com.example.mandelamoney;
 
+import android.content.Context;
+import android.content.Intent;
+
 public class MakePaymentController {
+    private IConfirmPaymentView confirmPaymentView;
+    private Context context;
+    public MakePaymentController(Context context) {
+        this.context = context;
+    }
+
+    public void setConfirmView(IConfirmPaymentView view) {
+        confirmPaymentView = view;
+    }
+
+    public void setContext(Context context) {
+        this.context = context;
+    }
+
+    public void handleConfirmPayment() {
+        /// confirms, and moves to next screen.
+        DataShare.send(this);
+    }
+
+    public void handleCancel() {
+        Intent intent = new Intent(context, DashboardActivity.class);
+        context.startActivity(intent);
+    }
 }
