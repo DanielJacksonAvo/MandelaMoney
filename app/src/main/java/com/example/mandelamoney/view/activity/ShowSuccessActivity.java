6da5e58e042c9c0955dc4fa7ea48631fc774ddd7
GlacierColt <lapel.tidbits.0c@icloud.com>
2025-08-03 20:09:42 +0200

    Refactor: Update MakePaymentController and ShowSuccessActivity for transaction status display
    This commit introduces changes to `MakePaymentController` to support displaying transaction status and updates `ShowSuccessActivity` to utilize this.
    
    **MakePaymentController.java:**
    - The `context` field is no longer final.
    - Added a new field `transactionStatusDisplayView` of type `ITransactionStatusDisplayView`.
    - Added a new public method `setTransactionStatusDisplayView(ITransactionStatusDisplayView transactionStatusDisplayView)` to set the view.
    - Added a new public method `setContext(Context context)` to update the controller's context.
    - Removed comments related to setting 2x zoom in `initializeCamera()`.
    - Removed comments indicating that scanning/data fetching and payment logic were executed on separate threads.
    
    **ShowSuccessActivity.java:**
    - In the `onCreate` method:
        - The object received from `DataShare.receive()` is now stored in a local variable `controller`.
        - If `controller` is an instance of `MakePaymentController`, its `setTransactionStatusDisplayView()` method is called with `this` (the `ShowSuccessActivity` instance).

--- app/src/main/java/com/example/mandelamoney/view/activity/ShowSuccessActivity.java
+++ app/src/main/java/com/example/mandelamoney/view/activity/ShowSuccessActivity.java
@@ -1,5 +1,6 @@
 package com.example.mandelamoney.view.activity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
@@ -15,14 +16,17 @@ import androidx.core.view.WindowInsetsControllerCompat;
 import com.example.mandelamoney.R;
 import com.example.mandelamoney.controller.MakePaymentController;
 import com.example.mandelamoney.controller.RequestPaymentController;
+import com.example.mandelamoney.model.Business;
+import com.example.mandelamoney.model.Student;
+import com.example.mandelamoney.model.Transaction;
+import com.example.mandelamoney.model.User;
 import com.example.mandelamoney.util.DataShare;
+import com.example.mandelamoney.util.MySQLConnector;
 import com.example.mandelamoney.view.Iface.ITransactionStatusDisplayView;
 
 public class ShowSuccessActivity extends AppCompatActivity implements ITransactionStatusDisplayView {
 
-    private TextView txtToname, txtFromname, txtTonumber, txtFromnumber, txtAmount;
-    private MakePaymentController makePaymentController;
-    private RequestPaymentController requestPaymentController;
+    private int transactionId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,85 +43,82 @@ public class ShowSuccessActivity extends AppCompatActivity implements ITransacti
             return insets;
         });
 
-        Object obj = DataShare.receive();
-        if (obj instanceof MakePaymentController) {
-            makePaymentController = (MakePaymentController) obj;
+        Object controller = DataShare.receive();
+        if (controller instanceof MakePaymentController) {
+            MakePaymentController makePaymentController = (MakePaymentController) controller;
             makePaymentController.setTransactionStatusDisplayView(this);
             makePaymentController.setContext(this);
-        } else {
-            if (obj instanceof RequestPaymentController) {
-                requestPaymentController = (RequestPaymentController) obj;
-                requestPaymentController.setTransactionStatusDisplayView(this);
-                requestPaymentController.setContext(this);
-            }
-
+        } else if (controller instanceof RequestPaymentController) {
+            RequestPaymentController requestPaymentController = (RequestPaymentController) controller;
         }
 
-        connectToUi();
+        transactionId = getIntent().getIntExtra("TRANSACTION_ID", 0);
 
-        if (makePaymentController != null) {
-            makePaymentController.loadTransactionStatusData();
-        } else if (requestPaymentController != null) {
-            requestPaymentController.loadTransactionStatusData();
+        if (transactionId != 0) {
+            new Thread(this::fetchDataAndPopulateUI).start();
         }
 
-
+        Button btnClose = findViewById(R.id.btn_generate_qr_success);
+        btnClose.setOnClickListener(v -> {
+            startActivity(new Intent(ShowSuccessActivity.this, DashboardActivity.class));
+            finish();
+        });
     }
 
-    private void connectToUi() {
-        txtToname = findViewById(R.id.txt_toname_success);
-        txtFromname = findViewById(R.id.txt_fromname_success);
-        txtTonumber = findViewById(R.id.txt_tonumber_success);
-        txtFromnumber = findViewById(R.id.txt_fromnumber_success);
-        txtAmount = findViewById(R.id.txt_amount_success);
-        Button btnClose = findViewById(R.id.btn_generate_qr_success);
-        configureCloseButton(btnClose);
+    private void fetchDataAndPopulateUI() {
+        final Transaction txnDetails = MySQLConnector.getTransactionDetailsFromProcedure(transactionId, this);
 
+        if (txnDetails != null) {
+            final User fromUser = MySQLConnector.getUserDetailsByEmail(txnDetails.getFromUser(), this);
+            final User toUser = MySQLConnector.getUserDetailsByEmail(txnDetails.getToUser(), this);
+
+            runOnUiThread(() -> {
+                displayAmount(txnDetails.getAmount());
+                if (fromUser != null) {
+                    if (fromUser instanceof Student) {
+                        displayFromUserName(((Student) fromUser).getStudentFullName());
+                        displayFromUserNumber(((Student) fromUser).getStudentNumber());
+                    } else if (fromUser instanceof Business) {
+                        displayFromUserName(((Business) fromUser).getBusinessName());
+                        displayFromUserNumber(((Business) fromUser).getBusinessVAT());
+                    }
+                }
+                if (toUser != null) {
+                    if (toUser instanceof Student) {
+                        displayToUserName(((Student) toUser).getStudentFullName());
+                        displayToUserNumber(((Student) toUser).getStudentNumber());
+                    } else if (toUser instanceof Business) {
+                        displayToUserName(((Business) toUser).getBusinessName());
+                        displayToUserNumber(((Business) toUser).getBusinessVAT());
+                    }
+                }
+            });
+        }
     }
 
     @Override
     public void displayToUserName(String name) {
-        txtToname.setText(name);
+        ((TextView) findViewById(R.id.txt_toname_success)).setText(name);
     }
 
     @Override
     public void displayFromUserName(String name) {
-        txtFromname.setText(name);
+        ((TextView) findViewById(R.id.txt_fromname_success)).setText(name);
     }
 
     @Override
     public void displayToUserNumber(String number) {
-        txtTonumber.setText(number);
+        ((TextView) findViewById(R.id.txt_tonumber_success)).setText(number);
     }
 
     @Override
     public void displayFromUserNumber(String number) {
-        txtFromnumber.setText(number);
+        ((TextView) findViewById(R.id.txt_fromnumber_success)).setText(number);
     }
 
+    @SuppressLint({"DefaultLocale", "SetTextI18n"})
     @Override
     public void displayAmount(double amount) {
-        txtAmount.setText("R " + String.format("%.2f", amount));
-    }
-
-    @Override
-    public void finishActivity() {
-        finish();
-    }
-
-    private void configureCloseButton(Button btnClose) {
-        btnClose.setOnClickListener(v -> {
-            if (makePaymentController != null) {
-                try {
-                    makePaymentController.handleCancel();
-
-                } catch (Exception ignore) {}
-            }
-            if (requestPaymentController != null) {
-                try {
-                    requestPaymentController.handleCancelButton();
-                } catch (Exception ignore) {}
-            }
-        });
+        ((TextView) findViewById(R.id.txt_amount_success)).setText("R " + String.format("%.2f", amount));
     }
 }
