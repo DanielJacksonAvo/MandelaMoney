96a2dadf303d08f08c09763213876085e8c5fa6b
GlacierColt <lapel.tidbits.0c@icloud.com>
2025-08-06 17:39:59 +0200

    Refactor: Remove unused method from IShowQRCodeRequestPaymentView
    This commit removes an empty line from the `IShowQRCodeRequestPaymentView` interface.
    
    **IShowQRCodeRequestPaymentView.java:**
    - Removed an unnecessary blank line between `displayQR(Bitmap bitmap)` and `finishActivity()`.

--- app/src/main/java/com/example/mandelamoney/view/Iface/IShowQRCodeRequestPaymentView.java
+++ app/src/main/java/com/example/mandelamoney/view/Iface/IShowQRCodeRequestPaymentView.java
@@ -5,6 +5,5 @@ import android.graphics.Bitmap;
 public interface IShowQRCodeRequestPaymentView {
     void displayToast(String message);
     void displayQR(Bitmap bitmap);
-
     void finishActivity();
 }
