9c13203e9720f2f37113d4cfcfffc701c5d88cd3
GlacierColt <lapel.tidbits.0c@icloud.com>
2025-08-13 12:25:44 +0200

    Feat: Add IProfileView interface
    This commit introduces a new interface `IProfileView` which defines methods for setting various user profile details in a view.
    
    **Specific Changes:**
    
    *   **`app/src/main/java/com/example/mandelamoney/view/Iface/IProfileView.java`:**
        *   Created a new interface `IProfileView`.
        *   Added methods to set:
            *   First name label (`setFirstNameLabel`)
            *   First name (`setFirstName`)
            *   Last name label (`setLastNameLabel`)
            *   Last name (`setLastName`)
            *   Email (`setEmail`)
            *   Student number label (`setStudentNumberLabel`)
            *   Balance (`setBalance`)
            *   Welcome name message (`setWelcomeName`)

---
+++ app/src/main/java/com/example/mandelamoney/view/Iface/IProfileView.java
@@ -0,0 +1,14 @@
+package com.example.mandelamoney.view.Iface;
+
+public interface IProfileView {
+    void setFirstNameLabel(String label);
+    void setFirstName(String firstName);
+    void setLastNameLabel(String label);
+    void setLastName(String lastName);
+    void setEmail(String email);
+    void setStudentNumberLabel(String label);
+    void setBalance(Float balance);
+    void setWelcomeName(String name);
+
+
+}
